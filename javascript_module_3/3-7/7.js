8a33ca098570c276799ac6f3fbfdf8168f1a7356
Miska Saari <miskajs190799@gmail.com>
2022-11-28 19:16:56 +0200

    3-7 tehty.

---
+++ javascript_module_3/3-7/7.js
@@ -0,0 +1,5 @@
+'use strict';
+const trigger = document.querySelector('#trigger');
+const target = document.querySelector('#target');
+trigger.addEventListener('mouseover', function () {target.src = 'img/picB.jpg'});
+trigger.addEventListener('mouseout', function () {target.src = 'img/picA.jpg'});
